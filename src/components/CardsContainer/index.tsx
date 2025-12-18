26a74fe3536b93ba84a1c6ef5eeceffc99ef2e1b
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-12-18 14:29:36 -0300

    :lipstick: style: adiciona efeito de vidro no container

--- src/components/CardsContainer/index.tsx
+++ src/components/CardsContainer/index.tsx
@@ -4,8 +4,25 @@ type CardsContainerProps = {
     children: React.ReactNode
 }
 
-export function CardsContainer({children}:CardsContainerProps){
-    return <div className="flex flex-col items-center justify-center md:flex-row md:flex-wrap gap-2 md:gap-4">
-        {children}
-    </div>
+export function CardsContainer({ children }: CardsContainerProps) {
+    return (
+        <div className="justify-center items-center flex">
+        <div
+            className="
+                flex flex-col items-center justify-center
+                md:flex-row md:flex-wrap
+                gap-2 md:gap-4
+                p-4 md:p-6
+                rounded-2xl
+                bg-white/10
+                backdrop-blur-md
+                border border-white/20
+                shadow-lg shadow-black/20
+                w-3/4 mb-4
+            "
+        >
+            {children}
+        </div>
+        </div>
+    );
 }
