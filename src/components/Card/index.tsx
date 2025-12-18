3259ba190c6801a86e119cb8d2b3d2906ab3c522
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-12-18 14:21:57 -0300

    :lipstick: style: adiciona borda neon aos cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,3 @@
-
 type CardProps = {
     href: string;
     image: string;
@@ -7,10 +6,25 @@ type CardProps = {
 
 export function Card({ href, image, alt }: CardProps) {
     return (
-            <div className="w-[350px] md:w-[450px] transform transition-transform duration-300 hover:scale-105 rounded-2xl border-2 overflow-hidden">
-                <a href={href} target="_blank">
-                    <img src={image} alt={alt}     className="w-full h-full object-cover"/>
-                </a>
-            </div>
-        );
+        <div
+            className="
+                w-[350px] md:w-[450px]
+                rounded-2xl
+                overflow-hidden
+                border-2 border-orange-400/30
+                transition-all duration-300
+                hover:scale-105
+                hover:border-orange-400
+                hover:shadow-[0_0_20px_rgba(251,146,60,0.85)]
+            "
+        >
+            <a href={href} target="_blank">
+                <img
+                    src={image}
+                    alt={alt}
+                    className="w-full h-full object-cover"
+                />
+            </a>
+        </div>
+    );
 }
