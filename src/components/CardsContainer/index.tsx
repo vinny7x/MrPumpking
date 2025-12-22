cd7b622a4d74ac677ab49495053bf9b2074e490c
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-12-22 18:56:13 -0300

    :bug: fix: ajusta responsividade do cardscontainer

--- src/components/CardsContainer/index.tsx
+++ src/components/CardsContainer/index.tsx
@@ -1,28 +1,28 @@
 import type React from "react";
 
 type CardsContainerProps = {
-    children: React.ReactNode
-}
+    children: React.ReactNode;
+};
 
 export function CardsContainer({ children }: CardsContainerProps) {
     return (
         <div className="justify-center items-center flex">
-        <div
-            className="
-                flex flex-col items-center justify-center
-                md:flex-row md:flex-wrap
-                gap-2 md:gap-4
-                p-4 md:p-6
-                rounded-2xl
-                bg-white/10
-                backdrop-blur-md
-                border border-white/20
-                shadow-lg shadow-black/20
-                w-3/4 mb-4
+            <div
+                className="
+               flex flex-col items-center justify-center
+                    md:flex-row md:flex-wrap
+                    gap-3 md:gap-4
+                    p-4 md:p-6
+                    rounded-2xl
+                    bg-white/10
+                    backdrop-blur-md
+                    border border-white/20
+                    shadow-lg shadow-black/20
+                    w-full md:w-3/4 mx-4
             "
-        >
-            {children}
-        </div>
+            >
+                {children}
+            </div>
         </div>
     );
 }
