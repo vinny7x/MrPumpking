e464d397a56e0e5b8de354e8507dfe927d05dafd
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-12-23 13:09:38 -0300

    ✨ feat: adicionan novos links

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ function App() {
     <>
       <HeroSection />
       <CardsContainer>
-        <Card image="/images/banners/pixgg.png" alt="imagem ai" href="https://pixgg.com/marcosbarrocal" />
-        <Card image="/images/banners/discord.png" alt="imagem ai" href="https://discord.gg/dDeJFDQ8zm" />
+        <Card image="/images/banners/sub.png" alt="Assinar mensalmente para vantagens exclusivas" href="https://livepix.gg/mrpumpking/little-pumpking" />
+        <Card image="/images/banners/donate.png" alt="Enviar uma doação ao MrPumpKing" href="https://livepix.gg/mrpumpking" />
+        <Card image="/images/banners/discord.png" alt="Entrar na comunidade Discord do MrPumpKing" href="https://discord.gg/dDeJFDQ8zm" />
       </CardsContainer>
     </>
 
